7cdc73e724e84621c5b0def4d8b63c04c42fd04e
name <email>
2024-11-10 21:20:17 +0700

    test2

--- app/config/config.php
+++ app/config/config.php
@@ -5,9 +5,9 @@ define('BASEURL', 'http://localhost/invensync/public');
 // database
 
 define('DB_HOST', 'localhost');
-define('DB_SERVICE_NAME', 'xe'); 
+define('DB_SERVICE_NAME', 'ORCL'); 
 define('DB_USER', 'SYSTEM');
-define('DB_PASS', '9b6pa5kt');
+define('DB_PASS', 'admin');
 
 
 ?>
