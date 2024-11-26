fe73501dfc1f843d6d2b8cfddb6b56ee8418378e
syahman <RIFATOK123@GMAIL.COM>
2024-11-26 12:35:03 +0700

    ketinggalan

--- app/views/employees/index.php
+++ app/views/employees/index.php
@@ -1,6 +1,6 @@
 <main class="flex-1 ml-64 p-8">
       <header class="flex justify-between items-center mb-6">
-        <h2 class="text-2xl font-semibold text-blue-500">Employees</h2>
+        <h2 class="text-2xl font-semibold">Employees</h2>
         <button class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">+ New Stock</button>
       </header>
 
@@ -13,8 +13,9 @@
           <option>Completed</option>
         </select>
       </div>
-
+      <h2 class="text-xl font-semibold pb-2">Admin Gudang</h2>
       <div class="bg-white rounded shadow">
+      
       <table class="w-full text-left border-collapse">
       <thead>
       <tr class="bg-gray-200 text-gray-600">
@@ -28,7 +29,7 @@
             </tr>
       </thead>
         <tbody>
-          <?php $no = 1; foreach( $data['users'] as $users ) : ?>
+          <?php $no = 1; foreach( $data['invusers'] as $users ) : ?>
             <tr class="hover:bg-gray-100">
               <td  scope="row" class="py-3 px-4 border"><?= $no++; ?></td>
               <td class="py-3 px-4 border"><?= $users['USER_ID']; ?></td>
@@ -40,7 +41,39 @@
           <?php endforeach; ?>
         </tbody>
       </table>
+      
       </div>
+      <h2 class="text-xl font-semibold pb-2">Admin Gudang</h2>
+      <div class="bg-white rounded shadow">
+      
+      <table class="w-full text-left border-collapse">
+      <thead>
+      <tr class="bg-gray-200 text-gray-600">
+              <th class="py-3 px-4 border">No.</th>
+              <th class="py-3 px-4 border">User Id</th>
+              <th class="py-3 px-4 border">Email</th>
+              <th class="py-3 px-4 border">Name</th>
+              <th class="py-3 px-4 border">Role</th>
+              <th class="py-3 px-4 border">Address</th>
+              <th class="py-3 px-4 border">Phone Number</th>
+            </tr>
+      </thead>
+        <tbody>
+          <?php $no = 1; foreach( $data['cashusers'] as $users ) : ?>
+            <tr class="hover:bg-gray-100">
+              <td  scope="row" class="py-3 px-4 border"><?= $no++; ?></td>
+              <td class="py-3 px-4 border"><?= $users['USER_ID']; ?></td>
+              <td class="py-3 px-4 border"><?= $users['EMAIL']; ?></td>
+              <td class="py-3 px-4 border"><?= $users['NAME']; ?></td>
+              <td class="py-3 px-4 border"><?= $users['ROLE']; ?></td>
+              <td class="py-3 px-4 border"><?= $users['ADDRESS']; ?></td>
+              <td class="py-3 px-4 border"><?= $users['PHONE_NUMBER']; ?></td>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
+      
+      </div>
+
     </main>
   </div>
 </body>
