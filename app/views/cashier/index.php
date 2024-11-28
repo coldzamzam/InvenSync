e7df316bf9f91ad35e352f2603e6090b9d9d2ae2
syahman <RIFATOK123@GMAIL.COM>
2024-11-28 21:31:46 +0700

    RALAT LAGI

--- app/views/cashier/index.php
+++ app/views/cashier/index.php
@@ -1,4 +1,4 @@
-<main class="flex-1 ml-64 p-8">
+<main class="flex-1 ml-64 mt-20 p-8">
   <header class="flex justify-between items-center mb-6">
     <h2 class="text-2xl font-semibold text-blue-500">Transaksi</h2>
     <button id="tambahBarangBtn" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">+ Tambah Barang</button>
